c6b4bc95936a70028f48b429ca32ba55c83bb890
Rafael <ruizr7788@gmail.com>
2021-05-05 15:02:40 -0700

    Finished business hours

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const imgCards = document.querySelectorAll(".landing_img_card");
+const days = document.querySelectorAll(".days");
 
-window.addEventListener("scroll", checkCards);
+const currentDate = new Date();
+const today = currentDate.getDate();
+const tomorrow = new Date(+currentDate + 24 * 60 * 60 * 1000);
+const day3 = new Date(+currentDate + 2 * 24 * 60 * 60 * 1000);
+const day4 = new Date(+currentDate + 3 * 24 * 60 * 60 * 1000);
+const day5 = new Date(+currentDate + 4 * 24 * 60 * 60 * 1000);
+const day6 = new Date(+currentDate + 5 * 24 * 60 * 60 * 1000);
+const day7 = new Date(+currentDate + 6 * 24 * 60 * 60 * 1000);
+const nextDays = [
+  [today],
+  [tomorrow.getDate()],
+  [day3.getDate()],
+  [day4.getDate()],
+  [day5.getDate()],
+  [day6.getDate()],
+  [day7.getDate()],
+];
 
-checkCards();
-function checkCards() {
-  const triggerBottom = (window.innerHeight / 5) * 4;
-
-  imgCards.forEach((card) => {
-    const cardTop = card.getBoundingClientRect().top;
-
-    if (cardTop < triggerBottom) {
-      card.classList.add("show");
-    } else {
-      card.classList.remove("show");
-    }
-  });
-}
+days.forEach((day, i) => {
+  day.textContent = nextDays[i];
+});
