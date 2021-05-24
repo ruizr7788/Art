5665673b7cf581f266b24c764bf1266e50bea427
Rafael <ruizr7788@gmail.com>
2021-05-23 19:49:39 -0700

    Modified landing page to have a different background. Added a fixed navbar. Changed business hours background image. All screens and devices were considered and modified as well

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// fixed navbar
+const navbar = document.getElementById("topbar");
+const businessLogo = document.getElementById("business_logo");
+
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-navbar");
+    businessLogo.src = "/landing-imgs/fixedNavLOGO.png";
+  } else {
+    navbar.classList.remove("fixed-navbar");
+    businessLogo.src = "/landing-imgs/nowlogo.png";
+  }
+});
+
+// dates for business hours page
 const days = document.querySelectorAll(".days");
 const weekday = document.querySelectorAll(".weekday");
 
