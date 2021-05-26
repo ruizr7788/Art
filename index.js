e0fcc6dac64a40c8c61fd53d03b19b560fedb076
Rafael <ruizr7788@gmail.com>
2021-05-26 13:17:57 -0700

    Temporary commit in attempt to stop VS code from crashing. In the process of applying functionality to the photography section top bar selector

--- index.js
+++ index.js
@@ -1,102 +1,42 @@
-// fixed navbar
-const navbar = document.getElementById("topbar");
-const businessLogo = document.getElementById("business_logo");
+// making photography page dynamic to selection type
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const labelPhotography = document.getElementById("label-photo");
+const labelMakeup = document.getElementById("label-makeup");
+const labelPaintings = document.getElementById("label-painting");
+const displayOptions = document.querySelectorAll(".display_option");
 
-window.addEventListener("scroll", () => {
-  const scrollHeight = window.pageYOffset;
-  const navHeight = navbar.getBoundingClientRect().height;
-  if (scrollHeight > navHeight) {
-    navbar.classList.add("fixed-navbar");
-    businessLogo.src = "/landing-imgs/fixedNavLOGO.png";
-  } else {
-    navbar.classList.remove("fixed-navbar");
-    businessLogo.src = "/landing-imgs/nowlogo.png";
+let currentOptionActive = 0;
+prevBtn.addEventListener("click", () => {
+  currentOptionActive--;
+  if (currentOptionActive < 1) {
+    currentOptionActive = 0;
   }
+  console.log(currentOptionActive);
+  updateBtns();
 });
 
-// photo navbar
-const allTypes = {
-  studio: [
-    "/photography/ameer-basheer-_FRKHdiPXYs-unsplash.jpg",
-    "/photography/angel-gonzlez-rSODThq-thM-unsplash.jpg",
-    "/photography/art-hauntington-0wRXAXqIp58-unsplash.jpg",
-    "/photography/jessica-felicio-_cvwXhGqG-o-unsplash.jpg",
-    "/photography/michael-afonso-3lbxyaAoXbU-unsplash.jpg",
-    "/photography/noah-buscher-11lDEHFy_hA-unsplash.jpg",
-    "/photography/pooja-chaudhary-bqnbKxiIGZI-unsplash.jpg",
-    "/photography/portrait.jpg",
-  ],
-  outdoors: [
-    "/photography/christopher-campbell-nEWGCi7gB8U-unsplash.jpg",
-    "/photography/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg",
-    "/photography/nathan-mcdine-AZMiGR4fInQ-unsplash.jpg",
-    "/photography/seth-doyle-R5tHd-aYmPs-unsplash.jpg",
-    "/photography/waterfall.jpg",
-    "/photography/sitting.jpg",
-    "/photography/shore.jpg",
-    "/photography/lakeside.jpg",
-  ],
-  creative: [
-    "/photography/yuvraj-singh-QQTk22IPBGk-unsplash.jpg",
-    "/photography/noah-buscher-11lDEHFy_hA-unsplash.jpg",
-    "/photography/tennis.jpg",
-    "/photography/selfie.jpg",
-    "/photography/mirror.jpg",
-    "/photography/divided.jpg",
-    "/photography/bricks.jpg",
-  ],
-};
-
-const photoSectionEl = document.getElementById("photography");
-
-const displayPhotos = function (photoObj) {
-  photoObj.forEach((photo) => {
-    const html = `
-    <div class="photographyCard">
-      <img src="${photo}" alt="" />
-    </div>`;
-    photoSectionEl.insertAdjacentHTML("beforeend", html);
-  });
-};
-displayPhotos(allTypes.studio);
-
-const clearCurrentPhotos = function () {
-  photoSectionEl.innerHTML = "";
-};
-
-// displaying and clearing
-const btns = document.querySelectorAll(".photoOption");
-
-btns.forEach((btn, i) => {
-  btn.addEventListener("click", () => {
-    if (i === 0) {
-      clearCurrentPhotos();
-      displayPhotos(allTypes.studio);
-    } else if (i === 1) {
-      clearCurrentPhotos();
-      displayPhotos(allTypes.outdoors);
-    } else if (i === 2) {
-      clearCurrentPhotos();
-      displayPhotos(allTypes.creative);
-    } else if (i === 3) {
-      clearCurrentPhotos();
-      displayPhotos(allTypes.userChoice);
-    }
-  });
+nextBtn.addEventListener("click", () => {
+  currentOptionActive++;
+  if (currentOptionActive > 3) {
+    currentOptionActive = 3;
+  }
+  console.log(currentOptionActive);
+  updateBtns();
 });
 
-// photography section fade-in
-const photoContainer = document.getElementById("photography_container");
+const selectCurrent = (label) => {
+  label.classList.remove("text-muted");
+  label.classList.add("active");
+};
 
-window.addEventListener("scroll", checkPhotoContainer);
-checkPhotoContainer();
-
-function checkPhotoContainer() {
-  const triggerHalf = (window.innerHeight / 5) * 4;
-  const photoContainerTop = photoContainer.getBoundingClientRect().top;
-  if (photoContainerTop < triggerHalf) {
-    photoContainer.style.opacity = 100;
+const updateBtns = () => {
+  if (currentOptionActive === 0) {
+    prevBtn.disabled = true;
+  } else if (currentOptionActive === 2) {
+    nextBtn.disabled = true;
   } else {
-    photoContainer.style.opacity = 0;
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
   }
-}
+};
---
+++ javascript/fixedNavbar.js
@@ -0,0 +1,15 @@
+// fixed navbar
+const navbar = document.getElementById("topbar");
+const businessLogo = document.getElementById("business_logo");
+
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-navbar");
+    businessLogo.src = "/landing-imgs/fixedNavLOGO.png";
+  } else {
+    navbar.classList.remove("fixed-navbar");
+    businessLogo.src = "/landing-imgs/nowlogo.png";
+  }
+});
