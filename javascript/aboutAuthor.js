673c3cf6d71e4896fb0c4d004dc7ad9e18a1f2b2
Rafael <ruizr7788@gmail.com>
2021-05-27 19:30:07 -0700

    Added an abouti author to the footer for screens bigger than 1400px

---
+++ javascript/aboutAuthor.js
@@ -0,0 +1,15 @@
+const tabs = document.querySelectorAll(".tab");
+const info = document.querySelectorAll(".info");
+
+tabs.forEach((tab, i) => {
+  tab.addEventListener("click", () => {
+    hideAll();
+    tab.classList.add("active");
+    info[i].classList.remove("hidden");
+  });
+});
+
+function hideAll() {
+  tabs.forEach((tab) => tab.classList.remove("active"));
+  info.forEach((info) => info.classList.add("hidden"));
+}
