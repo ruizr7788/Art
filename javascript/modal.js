441c743ef75eb974a6c20c3e985a4924cf1e77d7
Rafael <ruizr7788@gmail.com>
2021-05-27 17:56:42 -0700

    Modal now responsive for all screens and devices

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,12 +1,17 @@
 const btn_open = document.getElementById("open_modal");
 const btn_exit = document.getElementById("modal_btn");
 const modal = document.querySelector(".modal-contact");
+const modalID = document.getElementById(".modal_contact");
 
 btn_open.addEventListener("click", openClose);
 btn_exit.addEventListener("click", openClose);
 document.addEventListener("keydown", (event) => {
-  event.key === "Escape" ? openClose() : -1;
+  event.key === "Escape" ? close() : -1;
 });
+
 function openClose() {
   modal.classList.toggle("hidden");
+}
+function close() {
+  modal.classList.add("hidden");
 }
