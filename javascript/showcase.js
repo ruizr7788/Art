0320f9b6265ca77029272c46d573a0882e748ad5
Rafael <ruizr7788@gmail.com>
2021-05-29 22:03:46 -0700

    full responsiveness, full functionality for showcasing paintings page. Work left to do: add a modal for new cards

---
+++ javascript/showcase.js
@@ -0,0 +1,47 @@
+// selectors --------------------------------
+const cards = document.querySelectorAll(".card");
+const card_modal = document.querySelectorAll(".card-modal");
+const main_container = document.querySelector(".main");
+const btn_exit_modal = document.querySelectorAll(".card-modal-exit");
+
+// functions ---------------------------------
+let modalIsOpen = false;
+
+// add event listener to all 'cards'
+// run only if modal is open is false
+cards.forEach((card, i) => {
+  // when a card is selected, gets its position and target card_modal at position [i] and set display to grid to show modal
+  // set cards opacity to .2 when modal is shown
+  // add blur to 'main'
+
+  card.addEventListener("click", () => {
+    if (modalIsOpen === false) {
+      card_modal[i].style.display = "grid";
+      main_container.classList.add("blur");
+      opacity02();
+      modalIsOpen = true;
+    }
+  });
+});
+
+// add an event listener to exit button and on a click event close current modal
+btn_exit_modal.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    card_modal[i].style.display = "none";
+    main_container.classList.remove("blur");
+    opacity1();
+    modalIsOpen = false;
+  });
+});
+
+function opacity02() {
+  cards.forEach((card) => {
+    card.style.opacity = "0.1";
+  });
+}
+
+function opacity1() {
+  cards.forEach((card) => {
+    card.style.opacity = "1";
+  });
+}
