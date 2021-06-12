46e1759a69da63dc537dfcb26a26aff239e48e56
Rafael <ruizr7788@gmail.com>
2021-06-12 13:40:21 -0700

    full responsiveness for gallery tour page.

--- javascript/galleryTour.js
+++ javascript/galleryTour.js
@@ -4,8 +4,8 @@ const infoCont2 = document.getElementById("info_container2");
 const quoteCont = document.getElementById("quote_container");
 const galleryInfo = document.getElementById("gallery_tour_info");
 
+// INFO TRANSITION
 window.addEventListener("scroll", checkInfo);
-
 checkInfo();
 function checkInfo() {
   const triggerHalf = (window.innerHeight / 5) * 4;
@@ -20,5 +20,21 @@ function checkInfo() {
     infoCont1.style.transform = "translateY(15em)";
     infoCont2.style.transform = "translateY(15em)";
     quoteCont.style.transform = "translateY(15em)";
+  }
+}
+
+// INRO TRANSITION
+const galleryTour = document.getElementById("gallery_tour");
+const galleryIntro = document.getElementById("gallery_tour_introduction");
+
+window.addEventListener("scroll", checkIntro);
+checkIntro();
+function checkIntro() {
+  const triggerHalf = (window.innerHeight / 5) * 4;
+  const introContainerTop = galleryTour.getBoundingClientRect().top;
+  if (introContainerTop < triggerHalf) {
+    galleryIntro.style.transform = "translate(0)";
+  } else {
+    galleryIntro.style.transform = "translateY(15em)";
   }
 }
